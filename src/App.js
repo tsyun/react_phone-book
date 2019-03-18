1a6c421a5503904b929158f9236b94bbad0e468c
Your Name <you@your-domain.com>
2019-03-18 22:37:11 +0900

    누구든지 하는 리액트 7편:배열다루기(1) 생성과 렌더링

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
+// file: src/App.js
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PhoneForm from './components/PhoneForm'
+import PhoneInfoList from './components/PhoneInfoList';
 
 class App extends Component {
+
+  id = 2
+  state = {
+    information: [
+      {
+        id: 0,
+        name: '김민준',
+        phone: '010-0000-0000'
+      },
+      {
+        id: 1,
+        name: '홍길동',
+        phone: '010-0000-0001'
+      }
+    ]
+  }
+
+  handleCreate = (data) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.concat({ id: this.id++, ...data })
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <PhoneForm
+          onCreate={this.handleCreate}
+        />
+        <PhoneInfoList data={this.state.information}/>
       </div>
     );
   }
