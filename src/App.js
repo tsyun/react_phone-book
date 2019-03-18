b0e2740a50ba11cbfeaedd9a2af2c8c4ac1a36fc
Your Name <you@your-domain.com>
2019-03-18 23:39:24 +0900

    누구든지 하는 리액트 8편:배열다루기(2) 제거와 수정

--- src/App.js
+++ src/App.js
@@ -28,13 +28,36 @@ class App extends Component {
     })
   }
 
+  handleRemove = (id) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.filter(info => info.id !== id)
+    })
+  }
+
+  handleUpdate = (id, data) => {
+    const { information } = this.state;
+    this.setState({
+      information: information.map(
+        info => id === info.id
+          ? { ...info, ...data } // 새 객체를 만들어서 기존의 값과 전달받은 data 를 덮어씀
+          : info // 기존의 값을 그대로 유지
+      )
+    })
+  }
+
   render() {
+    const { information } = this.state;
     return (
       <div>
         <PhoneForm
           onCreate={this.handleCreate}
         />
-        <PhoneInfoList data={this.state.information}/>
+        <PhoneInfoList
+          data={information}
+          onRemove={this.handleRemove}
+          onUpdate={this.handleUpdate}  
+        />
       </div>
     );
   }
